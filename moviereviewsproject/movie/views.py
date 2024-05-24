066f0d343dc0163fc926dbc06d30ddd1a2c52352
Lubo <lmicov@gmail.com>
2024-05-24 21:17:44 +0300

    up to news app

--- moviereviewsproject/movie/views.py
+++ moviereviewsproject/movie/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
+
 
 def home(request):
     # return HttpResponse('<h1> Welcome to home page</h1>')
-    search_term = request.GET.get('search_movie')
-    return render(request, 'home.html', {'search_term': search_term})
+    search_term = request.GET.get('search_movie')     
+    # movies = Movie.objects.all()
+
+    if search_term:
+        movies = Movie.objects.filter(title__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    
+    return render(request, 'home.html', 
+                  {'search_term': search_term, 'movies': movies})
+
 
 def about(request):
     return HttpResponse('<h1> Welcome to About Page</h1>')
